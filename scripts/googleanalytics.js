26417f0f490dd23d64d31d931b6939bed95128f1
knight.gao <knight.gao@tineco.com>
2023-12-27 10:17:53 +0800

    feat: 修改埋点

--- scripts/googleanalytics.js
+++ scripts/googleanalytics.js
@@ -1,13 +1,15 @@
-hexo.extend.injector.register('head_begin', `<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WBG3S975');</script>
-<!-- End Google Tag Manager -->`, 'default');
+hexo.extend.injector.register('head_end', `<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9DS27J1J2Y"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-hexo.extend.injector.register('body_end', `<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-9DS27J1J2Y"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->`, 'default');
+  gtag('config', 'G-9DS27J1J2Y');
+</script>`, 'default');
+
+// hexo.extend.injector.register('body_end', `<!-- Google Tag Manager (noscript) -->
+// <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-9DS27J1J2Y"
+// height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+// <!-- End Google Tag Manager (noscript) -->`, 'default');
 
